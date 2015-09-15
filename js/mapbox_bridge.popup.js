1c90b8af519d6ed5729e39cd9325c0b8f4690d9d
Alexander Tkachev <alex.leksat@gmail.com>
2015-09-15 18:21:23 +0300

    Added hover support for popup (UBX-675)

--- js/mapbox_bridge.popup.js
+++ js/mapbox_bridge.popup.js
@@ -7,6 +7,7 @@
    */
   Drupal.MapboxPopup = {
     popups: function (groups, viewmode, settings) {
+      var eventType = settings.popup.popup_event || 'click';
 
       // go through each group, then through each layer
       $.each(groups, function(id, layers){
@@ -19,11 +20,16 @@
           layer.bindPopup(content, {
             minWidth: 150
           })
-            // Bind popups to a click event
-            .on('click', function(marker) {
+            // Bind popups to an event
+            .on(eventType, function(e) {
+
+              // If it's a hover event, we have to open the popup manually.
+              if (e.type == 'mouseover') {
+                layer.openPopup();
+              }
 
               // load the node with the supplied viewmode
-              $( '#custom-popup-id-' + marker.target._leaflet_id ).load('/' + settings.path_settings.language + '/mapbox_bridge_ajax_content/' + viewmode + '/' + marker.target.options.nid, function(content){
+              $( '#custom-popup-id-' + e.target._leaflet_id ).load('/' + settings.path_settings.language + '/mapbox_bridge_ajax_content/' + viewmode + '/' + e.target.options.nid, function(content){
                 var $this = $(this),
                     $content = $('> div:first-child', $this);
 
@@ -43,8 +49,10 @@
                 $this.removeClass('loading');
 
                 // center the newly clicked marker
-                var px = Drupal.Mapbox.map.project(marker.target._latlng); // find the pixel location on the map where the popup anchor is
-                    px.y -= marker.target._popup._container.clientHeight / 2; // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
+                if (e.type == 'click') {
+                  var px = Drupal.Mapbox.map.project(e.target._latlng); // find the pixel location on the map where the popup anchor is
+                  px.y -= e.target._popup._container.clientHeight / 2; // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
+                }
 
                 Drupal.Mapbox.map.panTo( Drupal.Mapbox.map.unproject(px), { animate: true }); // pan to new center
               });
